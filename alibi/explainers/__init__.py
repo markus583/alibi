abcdf03e509db14210695ae4128364c1f9516688
markus.frohmann <markus.frohmann@leftshift.one>
2021-11-03 15:48:47 +0100

    fix

--- alibi/explainers/__init__.py
+++ alibi/explainers/__init__.py
@@ -5,7 +5,6 @@ The 'alibi.explainers' module includes feature importance, counterfactual and an
 from .ale import ALE, plot_ale
 from .anchor_tabular import AnchorTabular, DistributedAnchorTabular
 from .anchor_image import AnchorImage
-from .cem import CEM
 from .cfproto import CounterfactualProto, CounterFactualProto  # noqa: F401 TODO: remove in an upcoming release
 from .counterfactual import Counterfactual, CounterFactual  # noqa: F401 TODO: remove in an upcoming release
 from .integrated_gradients import IntegratedGradients
